d33d095b39dc469f9214edfe77bc67da1388f18b
elian <elian118@naver.com>
2024-07-11 14:44:49 +0900

    About 페이지 2차 작업 - 배너 반응형 로직 추가

--- app/about/components/Banner.tsx
+++ app/about/components/Banner.tsx
@@ -5,9 +5,9 @@ import aboutImg from '@/public/images/about.png';
 
 export const Banner = () => {
   return (
-    <div className="flex flex-col gap-4 w-full justify-center items-center h-screen bg-blue-50">
-      <div className="py-8">
-        <FadeIn>
+    <div className="flex flex-col gap-4 w-full h-[700px] md:h-[800px] lg:h-screen justify-center items-center bg-blue-50">
+      <FadeIn>
+        <div className="py-8 max-w-[536px]">
           <Image
             width={536}
             height={402}
@@ -16,8 +16,9 @@ export const Banner = () => {
             src={aboutImg as string}
             alt="about recoble"
           />
-        </FadeIn>
-      </div>
+        </div>
+      </FadeIn>
+
       <p className="font-semibold text-3xl lg:text-4xl xl:text-5xl">Our Beginning</p>
       <p className="text-lg lg:text-xl xl:text-2xl w-10/12 sm:w-auto">
         레코블은 부푼 꿈을 안고 시작된&nbsp;
