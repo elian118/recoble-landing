32313d0aeca9f0198a45aa48cb509710a2eff9b3
elian <elian118@naver.com>
2024-07-16 21:40:38 +0900

    home - assert-recoble - 레이어 재조정

--- app/components/views/assert-recoble.tsx
+++ app/components/views/assert-recoble.tsx
@@ -6,24 +6,26 @@ import FooterInSection from '@/components/footer-in-section';
 const AssertRecoble = () => {
   return (
     <section className="assert-recoble-section">
-      <div className="assert-recoble-container">
-        <div className="assert-recoble-text content card-text">
-          <p className="assert-recoble-title">
-            모든 이커머스 플랫폼의
-            <br />
-            성공전략, <span className="font-semibold">레코블</span>
-          </p>
-          <p className="text-2xl font-semibold md:text-3xl xl:text-5xl lg:mt-6 text-center lg:text-start">
-            지금 바로 확인해보세요
-          </p>
-        </div>
-        <div className="pl-8 pt-6 lg:pt-0 md:pl-0 w-full md:w-1/2 flex justify-end content card-graph">
-          <Image
-            className="object-contain object-right-bottom"
-            src={assertRecobleSlide as StaticImageData}
-            alt="Recommand you Recoble!"
-            style={{ width: 'auto', height: 'auto' }}
-          />
+      <div className="px-2 pt-24 sm:px-12 h-full w-full">
+        <div className="assert-recoble-container">
+          <div className="assert-recoble-text content card-text">
+            <p className="assert-recoble-title">
+              모든 이커머스 플랫폼의
+              <br />
+              성공전략, <span className="font-semibold">레코블</span>
+            </p>
+            <p className="text-2xl font-semibold md:text-3xl xl:text-5xl lg:mt-6 text-center lg:text-start">
+              지금 바로 확인해보세요
+            </p>
+          </div>
+          <div className="pl-8 pt-6 lg:pt-0 md:pl-0 w-full md:w-1/2 flex justify-end content card-graph">
+            <Image
+              className="object-contain object-right-bottom"
+              src={assertRecobleSlide as StaticImageData}
+              alt="Recommand you Recoble!"
+              style={{ width: 'auto', height: 'auto' }}
+            />
+          </div>
         </div>
       </div>
       <div className="w-full">
