d5e6e69b8daff96f7caed7fb8b1fef1ef0bcd5cd
elian <elian118@naver.com>
2024-07-16 17:39:10 +0900

    pricing - 배경 재조정

--- app/pricing/components/views/plan-table-view.tsx
+++ app/pricing/components/views/plan-table-view.tsx
@@ -1,10 +1,17 @@
+'use client';
+
 import React from 'react';
 import { plans } from '@/app/pricing/constants';
 import { PlanTableElmView } from '@/app/pricing/components/views';
+import { useWinSize } from '@/libs/hooks';
 
 export const PlanTableView = () => {
+  const { winWidth } = useWinSize();
+
   return (
-    <div className="plan-table plan-table-align hidden sm:flex">
+    <div
+      className={`plan-table plan-table-align hidden sm:flex ${winWidth >= 1536 && winWidth <= 1740 ? 'z-10' : ''}`}
+    >
       {plans.map((plan, pIdx) => (
         <PlanTableElmView key={plan.title} plan={plan} pIdx={pIdx} />
       ))}
