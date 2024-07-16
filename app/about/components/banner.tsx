653f1d333aa5a8c2cdf95a7c9cdbe8d500c49d8c
elian <elian118@naver.com>
2024-07-16 21:14:26 +0900

    about - 캐러셀 높이 통일

--- app/about/components/banner.tsx
+++ app/about/components/banner.tsx
@@ -39,8 +39,8 @@ export const Banner = () => {
         </div>
       </FadeIn>
 
-      <p className="font-semibold text-3xl lg:text-4xl xl:text-6xl mb-3">Our Beginning</p>
-      <p className="text-lg lg:text-xl xl:text-3xl w-10/12 sm:w-auto lg:leading-relaxed xl:leading-relaxed">
+      <p className="font-semibold text-3xl sm:text-4xl xl:text-6xl mb-3">Our Beginning</p>
+      <p className="text-lg sm:text-2xl lg:text-2xl xl:text-3xl w-10/12 sm:w-auto lg:leading-relaxed xl:leading-relaxed">
         레코블은 부푼 꿈을 안고 시작된&nbsp;
         <br className="flex sm:hidden" />
         우리의 서비스를 운영하면서
@@ -49,7 +49,7 @@ export const Banner = () => {
         <br className="flex sm:hidden" />
         시작되었습니다.
       </p>
-      <p className="font-bold text-lg lg:text-xl xl:text-3xl mt-8 w-[300px] md:w-auto">
+      <p className="font-bold text-lg sm:text-2xl lg:text-2xl xl:text-3xl mt-8 w-[300px] md:w-auto">
         우리와 같은 문제들이 반복되지 않도록&nbsp;
         <br className="flex sm:hidden" />
         레코블을 사용해보세요!
